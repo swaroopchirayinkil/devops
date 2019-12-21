d16247a7d64b013ed3ec22ed088a153f22b88325
swaroopchirayinkil <swaroopchirayinkil@gmail.com>
2019-12-21 13:27:11 +0530

    new files

---
+++ if_elif_else.py
@@ -0,0 +1,20 @@
+
+
+
+x = int(input('enter the value'))
+
+if x==1:
+    print('one')
+
+if x==2:
+    print('two')
+
+if x==3:
+    print('three')
+
+if x==4:
+    print('four')
+
+if x==5:
+    print('five')
+
